971dad63838d3f6f4b9241fcfce525562f0a3210
s0a1m0x01 <cx3eno@gmail.com>
2026-09-02 00:40:43 +0530

    feat: add Site repository

---
+++ backend/src/main/java/com/zidio/keystone/repository/SiteRepository.java
@@ -0,0 +1,81 @@
+package com.zidio.keystone.repository;
+
+import com.zidio.keystone.domain.entity.Customer;
+import com.zidio.keystone.domain.entity.Site;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface SiteRepository extends JpaRepository<Site, Long> {
+    List<Site> findByCustomer(Customer customer);
+
+    List<Site> findByCustomerId(Long customerId);
+
+    List<Site> findByNameContainingIgnoreCase(String name);
+
+    List<Site> findByCustomerIdAndNameContainingIgnoreCase(
+            Long customerId,
+            String name
+    );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
