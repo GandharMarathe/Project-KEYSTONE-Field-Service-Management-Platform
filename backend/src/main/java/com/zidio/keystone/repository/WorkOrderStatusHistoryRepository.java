bc2f3cc7a3e0f7b707a3431ba2c3f3d686bd0f58
s0a1m0x01 <cx3eno@gmail.com>
2026-09-02 23:23:28 +0530

    feat: add WorkOrderStatusHistory service

--- backend/src/main/java/com/zidio/keystone/repository/WorkOrderStatusHistoryRepository.java
+++ backend/src/main/java/com/zidio/keystone/repository/WorkOrderStatusHistoryRepository.java
@@ -1,4 +1,4 @@
-package come.zidio.keystone.repository;
+package com.zidio.keystone.repository;
 
 import com.zidio.keystone.domain.entity.WorkOrderStatusHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -6,12 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface WorkOrderStatusHistoryRepository
-        extends JpaRepository<WorkOrderStatusHistoryRepository, Long> {
-    List<WorkOrderStatusHistoryRepository> findByWorkOrderIdOrderByChangedAtAsc(Long workOrderId);
+        extends JpaRepository<WorkOrderStatusHistory, Long> {
 
-    List<WorkOrderStatusHistoryRepository> findByWorkOrderIdOrderByChangedAtDesc(Long workOrderId);
-
-    List<WorkOrderStatusHistoryRepository> findByChangedById(Long userId);
+    List<WorkOrderStatusHistory> findByWorkOrderIdOrderByChangedAtAsc(
+            Long workOrderId
+    );
 }
 
 
